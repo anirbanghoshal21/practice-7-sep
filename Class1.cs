5229d8e3b710284dd6756c4813feb34af2b812b0
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-07 20:22:56 +0530

    Problem 1

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using Delegates_library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_library
+{
+    delegate string delegate_method(string str);
+   
+}
+public class Len
+    {
+        static string Length(string s)
+        {
+            string temp = "";
+            int i, j;
+            Console.WriteLine("Length String...");
+            for (j = 0, i = s.Length - 1;
+                i >= 0;i--, j++)
+                temp += s[i];
+            return temp;
+        }
+       
+        }
+    
+
