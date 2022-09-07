540d06d2d6c4ed91f611ebe6e8b7be1b02e480ae
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-07 23:46:54 +0530

    prob 5

---
+++ Class5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_library
+{
+    public delegate void YearofServiceDelegate(int _joiningyear);
+    public class Employee
+    {
+        private int _joiningyear;
+
+        public int JoiningYear
+        {
+            get { return _joiningyear; }
+            set { _joiningyear = value; }
+        }
+        static void Main(string[] args)
+        {
+
+            Employee f = new Employee();
+            e.YearOfService
+            YearofServiceDelegate f = new YearofServiceDelegate(e.YearOfService);
+            f.Invoke();
+            f();
+
+        }
+    }
+}
